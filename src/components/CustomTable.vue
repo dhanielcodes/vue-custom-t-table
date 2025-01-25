0c640d3d8951989147d162c1144eb2eae62a3526
Daniel Adekoya <adekoyadaniel53@gmail.com>
2025-01-25 16:20:23 +0100

    init with table component

---
+++ src/components/CustomTable.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import { data } from '@/libs/data.ts'
+import enUS from '@arco-design/web-vue/es/locale/lang/en-us';
+const expandedKeys: any = ref([]);
+
+
+const columns = [{
+  title: 'Name',
+  dataIndex: 'name',
+}, {
+  title: 'Salary',
+  dataIndex: 'salary',
+}, {
+  title: 'Address',
+  dataIndex: 'address',
+}, {
+  title: 'Email',
+  dataIndex: 'email',
+}];
+
+
+const rowSelection = reactive({
+  type: 'checkbox',
+  showCheckedAll: true,
+  onlyCurrent: false,
+});
+
+
+</script>
+
+<template>
+  <div>
+    <a-config-provider :locale="enUS">
+      <a-table class="table" :columns="columns" :data="data" v-model:expandedKeys="expandedKeys"
+        :row-selection="rowSelection" />
+    </a-config-provider>
+  </div>
+</template>
+
+<style scoped>
+.table {
+  max-width: 1400px;
+  width: 90%;
+  margin: 0 auto;
+}
+
+.arco-icon-plus {
+  display: none !important;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import CustomTable from '@/components/CustomTable.vue'
+import NestedTable from '@/components/NestedTable.vue';
+
+</script>
+
+<template>
+  <div>
+    <CustomTable />
+
+  </div>
+</template>
